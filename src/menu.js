3a84601c09d12cbce5a0a3a88e9cd526fd342170
Tibarek Yuggie <ttibarek@gmail.com>
2025-09-07 11:48:44 +0300

    update menuItems in menu module to be function

--- src/menu.js
+++ src/menu.js
@@ -4,51 +4,55 @@ import latte from "./images/steaming-cup-coffee.jpg";
 import muffin from "./images/muffin.jpg";
 import espresso from "./images/espresso.jpg"
 
-const menuItems = [
-  {
-    name: "Cappuccino",
-    description: "A classic Italian coffee made with espresso, steamed milk, and foam.",
-    price: "$4.50",
-    image: cappuccino
-  },
-  {
-    name: "Latte",
-    description: "Espresso with steamed milk and a light layer of foam on top.",
-    price: "$4.00",
-    image: latte
-  },
-  {
-    name: "Muffin",
-    description: "Freshly baked muffin, perfect for a light snack.",
-    price: "$2.00",
-    image: muffin
-  },
-  {
-    name: "Espresso",
-    description: "A strong and rich coffee shot served black.",
-    price: "$2.50",
-    image: espresso
-  }
-  
-];
+const items = function () {
+  const menuItems = [
+    {
+      name: "Cappuccino",
+      description: "A classic Italian coffee made with espresso, steamed milk, and foam.",
+      price: "$4.50",
+      image: cappuccino
+    },
+    {
+      name: "Latte",
+      description: "Espresso with steamed milk and a light layer of foam on top.",
+      price: "$4.00",
+      image: latte
+    },
+    {
+      name: "Muffin",
+      description: "Freshly baked muffin, perfect for a light snack.",
+      price: "$2.00",
+      image: muffin
+    },
+    {
+      name: "Espresso",
+      description: "A strong and rich coffee shot served black.",
+      price: "$2.50",
+      image: espresso
+    } 
+  ];
+  return menuItems;
+}
 
 export const menu = function() {
-    const content = document.querySelector("#content");
-    const menuContainer = document.createElement("div");
-    menuContainer.classList.add("menu-container");
 
-    menuItems.forEach(item => {
-        const card = document.createElement('div');
-        card.classList.add('card');
+  const menuItem = new items();
+  const content = document.querySelector("#content");
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menu-container");
 
-        card.innerHTML = `
-            <img src="${item.image}" alt="${item.name}">
-            <h3>${item.name}</h3>
-            <p>${item.description}</p>
-            <p class="price">${item.price}</p>
-            `;
+  menuItem.forEach(item => {
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-        menuContainer.appendChild(card);
+    card.innerHTML = `
+      <img src="${item.image}" alt="${item.name}">
+      <h3>${item.name}</h3>
+      <p>${item.description}</p>
+      <p class="price">${item.price}</p>
+      `;
+
+      menuContainer.appendChild(card);
     });
     content.appendChild(menuContainer);
 
